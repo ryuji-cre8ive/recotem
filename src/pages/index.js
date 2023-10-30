8b7ba9fd1f44289c28a1bb58a0e299ccd3213a98
ryuji-cre8ive <ryujisoccer0327@gmail.com>
2023-10-30 14:16:50 +0800

    feat(new): search consoleの確認

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,24 +10,26 @@ import styles from "./index.module.css";
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+    <head>
       <meta
         name="google-site-verification"
         content="_NdjN27EKPzIYUJeUtFVWPXfgvEtC-uwNY0FWDdpIwQ"
       />
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
-          >
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+      <header className={clsx("hero hero--primary", styles.heroBanner)}>
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className="button button--secondary button--lg"
+              to="/docs/intro"
+            >
+              Docusaurus Tutorial - 5min ⏱️
+            </Link>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </head>
   );
 }
 
