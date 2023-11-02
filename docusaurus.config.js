91ed3772b62c91cdec096b464dc04cf519e2a0e9
ryuji-cre8ive <ryujisoccer0327@gmail.com>
2023-11-02 17:52:05 +0800

    sitemapの更新頻度修正

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         sitemap: {
-          changefreq: "daily",
+          changefreq: "always",
           priority: 0.5,
           ignorePatterns: ["/tags/**"],
           filename: "sitemap.xml",
